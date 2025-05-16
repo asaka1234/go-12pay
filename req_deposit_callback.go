8874381c49b3f0628f526c1ec4c0f7fc452d8923
cyrildou <cyrildou@live.cn>
2025-05-16 23:17:28 +0800

    up

--- req_deposit_callback.go
+++ req_deposit_callback.go
@@ -1,7 +1,7 @@
 package go_12pay
 
 // 充值的回调处理(传入一个处理函数)
-func (cli *Client) DepositCallback(req One2PayDepositBackReq, processor func(One2PayDepositBackReq) One2PayDepositBackRsp) One2PayDepositBackRsp {
+func (cli *Client) DepositCallback(req One2PayDepositBackReq, processor func(One2PayDepositBackReq) error) error {
 	//验证签名
 	//TODO
 
