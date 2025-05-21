a177b087be9daea2b5c0e830147c6fbd973349ce
cyrildou <cyrildou@live.cn>
2025-05-21 19:01:06 +0800

    up

--- header.go
+++ header.go
@@ -2,14 +2,14 @@ package go_12pay
 
 func getHeaders() map[string]string {
 	return map[string]string{
-		"Content-Type": "application/jason",
+		"Content-Type": "application/json",
 	}
 }
 
 // notice:  input params is fixed
 func getAuthHeaders(auth string, channel string, device string, partnerCode string) map[string]string {
 	return map[string]string{
-		"Content-Type":  "application/jason",
+		"Content-Type":  "application/json",
 		"channel":       channel, //"WEB",
 		"partner_code":  partnerCode,
 		"authorization": auth,
